10a002cfbc4595b83d3de46b4e207002fb851bc0
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-06-22 02:44:22 +0000

    Set up the bin executable file

--- lib/free_art_exhibits.rb
+++ lib/free_art_exhibits.rb
@@ -4,7 +4,7 @@ require 'nokogiri'
 
 require_relative "free_art_exhibits/version"
 require_relative "free_art_exhibits/cli"
-require_relative "free_art_exhibits/movie"
+require_relative "free_art_exhibits/exhibit"
 
 
 module FreeArtExhibits
